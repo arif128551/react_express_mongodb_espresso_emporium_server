02b6e18d2cc4f45a94015d30701fd200234f180f
Muhammed Arif <muhammedarif@Muhammeds-Mac-mini.local>
2025-05-16 12:35:55 +0600

    done delete operation

--- index.js
+++ index.js
@@ -32,9 +32,27 @@ async function run() {
 			res.send(result);
 		});
 
+		app.get("/coffees/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = {
+				_id: new ObjectId(id),
+			};
+			const result = await coffeesCollection.findOne(query);
+			res.send(result);
+		});
+
 		app.post("/coffees", async (req, res) => {
 			const newCoffee = req.body;
 			const result = await coffeesCollection.insertOne(newCoffee);
+			res.send(result);
+		});
+
+		app.delete("/coffees/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = {
+				_id: new ObjectId(id),
+			};
+			const result = await coffeesCollection.deleteOne(query);
 			res.send(result);
 		});
 
